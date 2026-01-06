d8f1f26fda926b8a98d1bd4b118c688ee50b17f8
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2026-01-06 21:03:51 +1100

    feat: init

---
+++ README.md
@@ -0,0 +1,62 @@
+# lazycp - copying for the terminally ill
+
+If you have a few too many panes, terminal windows, tabs, muxers, commandlines, and various other
+shell doodads open at the same time, and just want to get one damn file from the working directory
+of one to the working directory of another, then *lazycp* is the app for you.
+
+With a few aliases, typing out the fully qualified path to a directory you already have open in one
+of your multitude muxed terminal panes becomes as simple as:
+
+```sh
+# In one of your shells
+lcp file1.txt # lcp is aliased to `lcp copy`
+
+# In another one of your shells - lp is aliased to `lcp paste`
+lp # lp is aliased to `lcp paste`
+```
+
+And so it is done.
+
+## How it works
+
+Dead simple: any copy or move command is saved to a copy/move history file and thus referenced by
+the paste command.
+
+## Copying lazily over remote connections
+
+*lazycp* also works over remote connections via SSH (kind of)! If *lazycp* is installed on both host
+and client machines, the program is capable of reading the remote machine's copy/move history file
+and pasting whatever entry you specify to your local machine.
+
+So, if you have two panes open, one of them being a remote connection won't stop your laziness!
+
+```sh
+# On one machine
+lcp remotefile.txt
+# On another machine
+lp -h remote@192.168.1.100
+```
+
+You can imagine that setting up aliases for specific commonly used machines, alongside ssh-agent,
+would make the above process decently seamless.
+
+```sh
+# Paste from specific machine
+alias lpr="lp -h remote@192.168.1.86"
+
+# Paste from commonly used subnet with specified address (192.168.86.x)
+lps () {
+    lp -h "remote@192.168.86.$1"
+}
+
+lpr # paste from remote
+lps 100 # paste from 192.168.86.100
+```
+
+## Install
+
+Currently only for nerds who know how to build software.
+
+```sh
+cargo install .
+```
---
+++ src/main.rs
@@ -0,0 +1,105 @@
+#[cfg(windows)]
+compile_error!(
+    "Windows hath given you the Mouse and the File Explorer, use them with grace. (This crate is not supported on Windows)"
+);
+
+use std::{fs::OpenOptions, path::PathBuf};
+
+use clap::{Parser, Subcommand};
+
+/// The easiest way to copy for people who have too many terminals open.
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+    /// Storage location for copy history
+    #[arg(long, global = true, env = "LAZYCP_HISTFILE", value_parser = clap::value_parser!(PathBuf))]
+    histfile: Option<PathBuf>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Insert a list of files and directories into copy history for later pasting
+    #[command(aliases = &["cp", "c"])]
+    Copy {
+        #[arg(value_parser = clap::value_parser!(PathBuf))]
+        files: Vec<PathBuf>,
+    },
+    /// Insert a list of files or directories into move history for later pasting
+    #[command(aliases = &["mv", "m"])]
+    Move {
+        #[arg(value_parser = clap::value_parser!(PathBuf))]
+        files: Vec<PathBuf>,
+    },
+    /// Paste latest or specified copied files or diretories into the current directory
+    #[command(alias = "p")]
+    Paste {
+        /// Destination relative to current directory. A bit silly, but the option is there
+        #[arg(long, short, value_parser = clap::value_parser!(PathBuf))]
+        dest: Option<PathBuf>,
+        /// Advanced usecase - provide a host to copy from over SSH. Provide an alternative
+        /// --histfile if you did not copy with the same user as provided in the host string
+        #[arg(long, short)]
+        host: Option<String>,
+        /// Resolves symlinks to the original file when provided
+        #[arg(long, short)]
+        resolve_symlinks: Option<bool>,
+        /// Index of item within copy/move history to paste
+        #[arg(long, short)]
+        index: Option<usize>,
+    },
+    /// Show latest copy/move history
+    #[command(aliases = &["hist", "h"])]
+    History {
+        /// Number of history entries to show
+        #[arg(short)]
+        number: Option<usize>,
+    },
+    /// Clear copy/move history for default or specified history file
+    #[command(aliases = &["clr", "cl"])]
+    Clear,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+    let histfile = args
+        .histfile
+        .or({
+            if let Ok(cache) = std::env::var("XDG_CACHE_HOME") {
+                Some(PathBuf::from(format!("{cache}/lazycp/histfile")))
+            } else {
+                None
+            }
+        })
+        .or({
+            if let Ok(home) = std::env::var("HOME") {
+                Some(PathBuf::from(format!("{home}/.cache/lazycp/histfile")))
+            } else {
+                None
+            }
+        })
+        .expect(
+            "Could not find valid $XDG_CACHE_HOME or $HOME, please provide a valid --histfile location.",
+        );
+    std::fs::create_dir_all(&histfile.parent().unwrap())?;
+    let histfile = OpenOptions::new()
+        .create(true)
+        .read(true)
+        .append(true)
+        .open(&histfile)?;
+
+    match args.command {
+        Command::Copy { files } => todo!(),
+        Command::Move { files } => todo!(),
+        Command::Paste {
+            dest,
+            host,
+            resolve_symlinks,
+            index,
+        } => todo!(),
+        Command::Clear => todo!(),
+        Command::History { number } => todo!(),
+    }
+}
